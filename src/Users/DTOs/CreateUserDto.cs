079986236f86d09f8562d72254db3d5a4012869b
Lucas Soares <lucasdejesussoares@gmail.com>
2026-05-14 19:15:27 -0300

    Updated CreateUserDto.cs to return all columns

--- src/Users/DTOs/CreateUserDto.cs
+++ src/Users/DTOs/CreateUserDto.cs
@@ -4,6 +4,8 @@ namespace src.Users.DTOs;
 
 public class CreateUserDto
 {
+    public Guid? Id { get; set; }
+
     [Required]
     [MaxLength(20)]
     public string Name { get; set; } = string.Empty;
@@ -17,4 +19,9 @@ public class CreateUserDto
     [MinLength(8)]
     [MaxLength(128)]
     public string Password { get; set; } = string.Empty;
+
+    [MaxLength(10)]
+    public string? Role { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
 }

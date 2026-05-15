bbd1e46580a019a43e940fc540d07f99d7387f85
Lucas Soares <lucasdejesussoares@gmail.com>
2026-05-14 21:47:33 -0300

    Updated Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -21,6 +21,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.MapGet("/", () => "Servidor inciado com sucesso!");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
